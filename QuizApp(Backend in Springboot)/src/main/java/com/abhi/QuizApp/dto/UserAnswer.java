38877534e7deb4e77777b8aff533fa8ac7ad2361
Abhi-dot <134879334+Abhi-dot@users.noreply.github.com>
2024-12-27 16:24:16 +0530

    Add files via upload

---
+++ QuizApp(Backend in Springboot)/src/main/java/com/abhi/QuizApp/dto/UserAnswer.java
@@ -0,0 +1,24 @@
+package com.abhi.QuizApp.dto;
+
+public class UserAnswer {
+    private Long questionId;
+    private String userAnswer;
+
+    // Getters and setters
+    public Long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Long questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getUserAnswer() {
+        return userAnswer;
+    }
+
+    public void setUserAnswer(String userAnswer) {
+        this.userAnswer = userAnswer;
+
+}
+}
